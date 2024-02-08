31a66995dc5bc55003799821ab09e80697c15d53
Eugena <eugena.h12345@gmail.com>
2024-02-08 20:40:47 +0300

    add if a number is prime

--- games/primeGame.js
+++ games/primeGame.js
@@ -1,3 +1,29 @@
-export default () => {
+import {
+  getRandomNumber, formulateQuestion, getAnswer, getAllDivisors,
+  checkResult, sayCongratulations,
+} from '../src/index.js';
+import greeting from '../bin/cli.js';
 
+const getRightAnswer = (number) => {
+  const allNumberDivisor = getAllDivisors(number);
+  return allNumberDivisor.length > 2 ? 'no' : 'yes';
+};
+
+export default () => {
+  const name = greeting();
+
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  for (let i = 1; i <= 3; i += 1) {
+    const firstNumber = getRandomNumber(100);
+    const expressionForGame = `${firstNumber}`;
+    const question = formulateQuestion(expressionForGame);
+    const answer = getAnswer(question);
+    const rightAnswer = getRightAnswer(firstNumber);
+    const checkedResult = checkResult(rightAnswer, answer, name);
+    if (!checkedResult) {
+      return;
+    }
+  }
+  sayCongratulations(name);
 };
