b7d193613adc455f65a70714fa5183db90c1f2df
Eugena <eugena.h12345@gmail.com>
2024-02-14 22:35:48 +0300

    update isEvenGame with engine funk

--- src/games/isEvenGame.js
+++ src/games/isEvenGame.js
@@ -1,29 +1,16 @@
-import greeting from '../cli.js';
 import {
-  getRandomNumber, formulateQuestion, getAnswer, checkResult, sayCongratulations,
+  getRandomNumber, isEvenNumber, engine,
 } from '../index.js';
 
-const isEvenNumber = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
+const generalQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+const getGameData = () => {
+  const randomNumber = getRandomNumber();
+  const expressionForGame = `${randomNumber}`;
+  const rightAnswer = (isEvenNumber(randomNumber)) ? 'yes' : 'no';
+  return { expressionForGame, rightAnswer };
 };
 
 const isEvenGame = () => {
-  const name = greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 1; i <= 3; i += 1) {
-    const randomNumber = getRandomNumber(100);
-    const expressionForGame = `${randomNumber}`;
-    const question = formulateQuestion(expressionForGame);
-    const answer = getAnswer(question);
-    const rightAnswer = (isEvenNumber(randomNumber)) ? 'yes' : 'no';
-    const checkedResult = checkResult(rightAnswer, answer, name);
-    if (!checkedResult) {
-      return;
-    }
-  }
-  sayCongratulations(name);
+  engine(generalQuestion, getGameData);
 };
 export default isEvenGame;
