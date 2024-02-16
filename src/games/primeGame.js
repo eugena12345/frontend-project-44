c090a9e28b74bafc8a4a9ea9ca327453c4faab8d
Eugena <eugena.h12345@gmail.com>
2024-02-16 22:49:28 +0300

    update src/games/primrGame with engine

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,29 +1,19 @@
 import {
-  getRandomNumber, formulateQuestion, getAnswer, getAllDivisors,
-  checkResult, sayCongratulations,
+  getRandomNumber, getAllDivisors, engine,
 } from '../index.js';
-import greeting from '../cli.js';
 
+const generalQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getRightAnswer = (number) => {
   const allNumberDivisor = getAllDivisors(number);
   return allNumberDivisor.length > 2 ? 'no' : 'yes';
 };
+const getGameData = () => {
+  const firstNumber = getRandomNumber();
+  const expressionForGame = `${firstNumber}`;
+  const rightAnswer = getRightAnswer(firstNumber);
+  return { expressionForGame, rightAnswer };
+};
 
 export default () => {
-  const name = greeting();
-
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-  for (let i = 1; i <= 3; i += 1) {
-    const firstNumber = getRandomNumber(100);
-    const expressionForGame = `${firstNumber}`;
-    const question = formulateQuestion(expressionForGame);
-    const answer = getAnswer(question);
-    const rightAnswer = getRightAnswer(firstNumber);
-    const checkedResult = checkResult(rightAnswer, answer, name);
-    if (!checkedResult) {
-      return;
-    }
-  }
-  sayCongratulations(name);
+  engine(generalQuestion, getGameData);
 };
