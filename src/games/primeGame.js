7feed44ddada9a00234cebd8a925cdb1ad35fe26
Eugena <eugena.h12345@gmail.com>
2024-02-22 20:42:08 +0300

    add func getAllDivisors

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,6 +1,30 @@
 import {
-  getRandomNumber, getAllDivisors, engine,
+  getRandomNumber, engine,
 } from '../index.js';
+
+const getAllDivisors = (number) => {
+  if (number === 1) {
+    return [1];
+  }
+  const divisorsColl = [1, number];
+  const iter = (divisor) => {
+    if (divisor < number / 2) {
+      if (number % divisor === 0) {
+        const secondDivisor = number / divisor;
+        if (!divisorsColl.includes(divisor)) {
+          divisorsColl.push(divisor);
+        }
+        if (!divisorsColl.includes(secondDivisor)) {
+          divisorsColl.push(secondDivisor);
+        }
+      }
+      iter(divisor + 1);
+    }
+  };
+  iter(2);
+  divisorsColl.sort((a, b) => a - b);
+  return divisorsColl;
+};
 
 const generalQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getRightAnswer = (number) => {
